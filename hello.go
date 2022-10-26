bf66fa4195e8da0e0ec0d4885e632a101475d791
Alexeyka <alexeykaz11@gmail.com>
2022-10-26 13:33:52 +0300

    +

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"https://github.com/Aleale16/gotoschool/functionlib.go"
+)
 
 func main() {
     fmt.Println("Hello, world!!!")
+	sum := functionlib.Sum(2, 3, 5, 1, 2, 57) 
 } 
