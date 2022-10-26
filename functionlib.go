50a18cd8cc4c0aa25fde39a79b09745c532dab3f
Alexeyka <alexeykaz11@gmail.com>
2022-10-26 13:27:53 +0300

    +

---
+++ functionlib.go
@@ -0,0 +1,16 @@
+package calculator
+
+import "fmt"
+
+func Sum(x ...int) (res int) {
+    for _, v := range x {
+        res += v
+    }
+    return
+} 
+
+func main() {
+
+	sum := Sum(2, 3, 5, 1, 2, 57) 
+	fmt.Println(sum)
+}
