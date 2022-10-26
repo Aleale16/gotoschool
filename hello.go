9d284392976b9fc68332d3454660674a2eaa72ea
Alexeyka <alexeykaz11@gmail.com>
2022-10-26 18:32:27 +0300

    +

--- hello.go
+++ hello.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Aleale16/gopkg"
+	//"github.com/Aleale16/gopkg"
 )
 
 func main() {
     fmt.Println("Hello, world!!!")
-	sum := gopkg.Sum(2, 3, 5, 1, 2, 57) 
-	fmt.Println(sum)
+	//sum := gopkg.Sum(2, 3, 5, 1, 2, 57) 
+	//fmt.Println(sum)
 } 
