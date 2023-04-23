078393c6be08d3e0c4c481b64c4b05b011d0f7b2
Alexeyka <alexeykaz11@gmail.com>
2023-04-23 18:50:58 +0300

    new file:   db.db     new file:   demo/proto/demo.pb.go 	new file:   demo/proto/demo.proto 	new file:   demo/proto/demo_grpc.pb.go 	new file:   demo/proto/server/go.mod 	new file:   demo/proto/server/go.sum 	new file:   demo/proto/server/main.go 	new file:   demo/proto/server/server.go 	new file:   errcheckanalyzer/errcheckanalyzer.go 	new file:   errcheckanalyzer/errcheckanalyzer_test.go 	new file:   errcheckanalyzer/testdata/pkg1/pkg1.go 	new file:   go.mod 	new file:   go.sum 	new file:   go.work 	deleted:    hello.go 	new file:   main copy.go.bak 	new file:   main copy1.go.bak 	new file:   main copy10.go.bak 	new file:   main copy2.go.bak 	new file:   main copy3.go.bak 	new file:   main copy4.go.bak 	new file:   main copy5.go.bak 	new file:   main copy6.go.bak 	new file:   main copy7.go.bak 	new file:   main copy8.go.bak 	new file:   main copy9.go.bak 	new file:   main.go.bak 	new file:   main_test copy.go.bak 	new file:   main_test.go 	new file:   staticcheck.conf 	new file:   storage/storage.go.bak 	new file:   test.proto 	new file:   url_shortener_get_put_delete.go.bak

---
+++ demo/proto/server/main.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	pb "demo/demo/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// определяем порт для сервера
+	listen, err := net.Listen("tcp", ":3200")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// создаём gRPC-сервер без зарегистрированной службы
+	s := grpc.NewServer()
+	// регистрируем сервис
+	pb.RegisterUsersServer(s, &UsersServer{})
+
+	fmt.Println("Сервер gRPC начал работу")
+	// получаем запрос gRPC
+	if err := s.Serve(listen); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module demo
+
+go 1.19
+
+require (
+	golang.org/x/tools v0.8.0
+	google.golang.org/grpc v1.54.0
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+)
